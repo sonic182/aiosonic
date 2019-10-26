58a10d2f2ba675f6652a5b18a22199fc367d6ea1
sonic182 <johander1822@gmail.com>
2019-10-26 09:42:52 +0200

    Fix timeouts

--- aiosonic/timeout.py
+++ aiosonic/timeout.py
@@ -3,9 +3,17 @@
 class Timeouts():
     """Timeouts class wrapper."""
 
-    def __init__(self, sock_connect: int = 5, sock_read: int = 60,
-                 pool_acquire: int = None, request_timeout: int = None):
-        """Initialize."""
+    def __init__(self, sock_connect: float = 5, sock_read: float = 30,
+                 pool_acquire: float = None, request_timeout: float = 60):
+        """Timeouts.
+
+        Arguments:
+            * sock_connect(float): time for establish connection to server
+            * sock_read(float): time until get first read
+            * pool_acquire(float): time until get connection from
+              connection's pool
+            * request_timeout(float): time until complete request.
+        """
         self.sock_connect = sock_connect
         self.sock_read = sock_read
         self.pool_acquire = pool_acquire
