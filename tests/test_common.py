6c7a61e89297b9363483fa13f417a6b6067dbe02
sonic182 <johander1822@gmail.com>
2020-11-21 23:54:23 +0100

    adding test file

---
+++ tests/test_common.py
@@ -0,0 +1,15 @@
+
+from aiosonic import HttpHeaders
+
+
+def test_headers_retrival():
+    """Test reading header with more than one ":" char ocurrence."""
+    sample_header = b'Report-To: { "group": "wm_nel", "max_age": 86400, "endpoints": [{ "url": "https://intake-logging.wikimedia.org/v1/events?stream=w3c.reportingapi.network_error&schema_uri=/w3c/reportingapi/network_error/1.0.0" }] }\r\n'
+    assert len(HttpHeaders._clear_line(sample_header)) == 2
+
+
+def test_headers_retrival_common():
+    """Test reading header with more than one ":" char ocurrence."""
+    res = [b'Authorization', b'Bearer foobar']
+    sample_header = b': '.join(res) + b'\r\n'
+    assert HttpHeaders._clear_line(sample_header) == res
