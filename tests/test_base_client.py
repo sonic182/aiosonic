ca6a0aed27020ae4ba505e1a59b00cb21dc87617
Johanderson <johander1822@gmail.com>
2025-03-10 23:58:43 +0100

    fix

--- tests/test_base_client.py
+++ tests/test_base_client.py
@@ -28,11 +28,11 @@ async def test_wrapper_delete_http_serv(http_serv):
       - Verify that a DELETE request returns "deleted".
     """
 
-    class TestWrapperClient(AioSonicBaseClient):
+    class TestWrapperClient2(AioSonicBaseClient):
         base_url = http_serv
         pass
 
-    client = TestWrapperClient()
+    client = TestWrapperClient2()
 
     response = await client.delete("/delete")
     assert isinstance(response, str), "Expected a string response"
